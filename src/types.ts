f184ebdf9fb21ce9d996ee3176fd06ca49bffd78
fraulueneburg <github@fraulueneburg.de>
2025-05-01 20:12:18 +0200

    cleaned up types

--- src/types.ts
+++ src/types.ts
@@ -12,31 +12,3 @@ export type ListData = {
 	title: string
 	color: string
 }
-
-export type LinkProps = {
-	title: string
-	hideTitle?: boolean
-	iconBefore?: ReactNode
-	iconAfter?: ReactNode
-	href: string
-	target?: string
-	onClick?: (event: React.MouseEvent<HTMLAnchorElement>) => void
-	ariaCurrent?: 'page' | 'step'
-}
-
-export type ButtonProps = {
-	title: string
-	hideTitle?: boolean
-	disabled?: boolean
-	iconBefore?: ReactNode
-	iconAfter?: ReactNode
-	className?: string
-	onClick: (event: React.MouseEvent<HTMLButtonElement>) => void
-	size?: 'sm' | 'md' | 'lg'
-	unstyled?: boolean
-}
-
-export type DropdownProps = {
-	isOpen: boolean
-	toggleButton: ReactNode
-}
