186a62ebff714fb0d649babe9f16cc96daf7b514
fraulueneburg <github@fraulueneburg.de>
2025-05-16 19:49:33 +0200

    style: switched order of menu items

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -130,10 +130,10 @@ export default function List({ data, tasks, isNew }: ListProps) {
 							<Submenu title={'list actions'} hideTitle={true} icon={<IconSubmenu />}>
 								<ul>
 									<li>
-										<Button title="delete" ariaLabel="delete list" onClick={handleDeleteList} size="sm" />
+										<Button title="rename" ariaLabel="rename list" onClick={() => setRenameMode(true)} size="sm" />
 									</li>
 									<li>
-										<Button title="rename" ariaLabel="rename list" onClick={() => setRenameMode(true)} size="sm" />
+										<Button title="delete" ariaLabel="delete list" onClick={handleDeleteList} size="sm" />
 									</li>
 								</ul>
 							</Submenu>
