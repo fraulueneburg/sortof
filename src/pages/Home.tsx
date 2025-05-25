d03d9a58db04c332e59c274601c97810fd86d7d5
fraulueneburg <github@fraulueneburg.de>
2025-05-25 12:24:06 +0200

    refactor (Home): moved FormNewList into separate component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,15 @@
 import { useState } from 'react'
 import { DndContext, DragEndEvent, useSensor, useSensors, MouseSensor, TouchSensor } from '@dnd-kit/core'
-import { nanoid } from 'nanoid'
 import useListContext from '../hooks/useListContext'
 
-import { ListData, TaskData } from '../types'
+import { TaskData } from '../types'
 
 import FormNewTask from '../components/FormNewTask'
 import List from '../components/List'
-import Button from '../components/Button'
-import { PlusIcon as IconAdd } from '@phosphor-icons/react'
+import FormNewList from '../components/FormNewList'
 
 export default function Home() {
 	const { allTasksArr, setAllTasksArr, listsArr, setListsArr } = useListContext()
-
-	const [newListId, setNewListId] = useState<string | null>(null)
-
-	const handleCreateNewList = () => {
-		const newId = nanoid()
-
-		const emptyList: ListData = {
-			_id: newId,
-			title: '',
-			color: 'purple',
-		}
-
-		setListsArr([emptyList, ...listsArr])
-		setNewListId(newId)
-	}
 
 	function handleDragEnd(event: DragEndEvent) {
 		const { active, over } = event
@@ -60,27 +43,14 @@ export default function Home() {
 	return (
 		<>
 			<h1>What do you need to do today?</h1>
+			<FormNewTask />
 
 			<DndContext onDragEnd={handleDragEnd}>
 				<>
-					<Button
-						className="btn-icon-only"
-						title="create new list"
-						hideTitle={true}
-						iconBefore={<IconAdd />}
-						onClick={handleCreateNewList}
-						size="lg"
-					/>
+					<FormNewList />
 					<div className="list-container">
 						{listsArr.map((col) => {
-							return (
-								<List
-									key={col._id}
-									data={col}
-									isNew={col._id === newListId}
-									tasks={allTasksArr.filter((task) => task.list === col._id)}
-								/>
-							)
+							return <List key={col._id} data={col} tasks={allTasksArr.filter((task) => task.list === col._id)} />
 						})}
 					</div>
 				</>
